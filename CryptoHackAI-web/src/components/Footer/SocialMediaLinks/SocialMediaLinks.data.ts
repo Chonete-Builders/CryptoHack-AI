4ea306ab65f069bfbbe3b0572caec3b1410247dc
Nan0ck <sgha10@hotmail.com>
2025-02-21 08:57:02 -0600

    [4] Footer

---
+++ CryptoHackAI-web/src/components/Footer/SocialMediaLinks/SocialMediaLinks.data.ts
@@ -0,0 +1,5 @@
+export const SOCIAL_MEDIA_URLS = {
+  Instagram: 'https://www.instagram.com/cryptoai',
+  LinkedIn: 'https://www.linkedin.com/company/cryptoai',
+  Discord: 'https://www.discord.com/cryptoai',
+};
