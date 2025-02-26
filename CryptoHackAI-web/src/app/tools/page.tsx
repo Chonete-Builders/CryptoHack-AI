57ddda078d74b15ef94adbf2ba955569c3003db8
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2025-02-26 20:25:15 +0100

    Update page.tsx

--- CryptoHackAI-web/src/app/tools/page.tsx
+++ CryptoHackAI-web/src/app/tools/page.tsx
@@ -2,7 +2,7 @@ import ToolsPage from "../components/toolscomponent/ToolsPage";
 
 export default function Tools() {
     return ( 
-        // Created Tools screen - Issue #8: Tools Page Implementation
+        
         <div>
             <ToolsPage />
         </div>
